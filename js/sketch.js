1cee4d94ab993f0b3021b20f8056c0a480df65d8
Jkilker <116875574+Jkilker@users.noreply.github.com>
2022-11-08 12:10:27 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -6,6 +6,15 @@ function setup() {
 function draw() {
     background(220);
 
-    fill(255, 255, 255);
+    fill(0, 0, 0)
+    rect(290, 50, 220, 700)
+
+    fill(255, 0, 0)
+    ellipse(400, 170, 200)
+
+    fill(255, 255, 0);
     ellipse(400, 400, 200);
+
+    fill(0, 255, 0)
+    ellipse(400, 630, 200)
 }
